0aac5cec5f76e22703e26f0974592d3de38a50ad
foxynprog <134656116+foxynprog@users.noreply.github.com>
2024-03-07 17:17:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/sXCQJK1P/Zorro-de-minecraft-removebg-preview.png')
+}
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+
+function draw() {
+ image(video, 0, 0, 300, 300);
+ //circle(noseX, noseY, 20);
+ image(clown_nose, noseX, noseY, 150, 150);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
+
+function modelLoaded() {
+  console.log('PoseNet está inicializando');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-70;
+    noseY = results[0].pose.nose.y-80;
+
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+  }
+}
